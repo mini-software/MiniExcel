75c884934f9d4f1f8a2495bc458bca08701f8dc8
wei <shps951002@gmail.com>
2021-03-03 13:08:33 +0800

    Add:VS MiniExcel Project

---
+++ src/MiniExcel/ExcelNamespaces.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniExcel
+{
+    using System.Xml.Linq;
+
+    internal static class ExcelNamespaces
+    {
+        internal static XNamespace excelNamespace = XNamespace.Get("http://schemas.openxmlformats.org/spreadsheetml/2006/main");
+        internal static XNamespace excelRelationshipsNamepace = XNamespace.Get("http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+    }
+}
---
+++ src/MiniExcel/ZipPackageInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniExcel
+{
+    using System.IO.Packaging;
+
+    internal class ZipPackageInfo
+    {
+        public string Xml { get; set; }
+        public string ContentType { get; set; }
+        public CompressionOption CompressionOption { get; set; } = CompressionOption.Normal;
+        public ZipPackageInfo(string xml, string contentType, CompressionOption CompressionOption = CompressionOption.Normal)
+        {
+            Xml = xml;
+            ContentType = contentType;
+        }
+    }
+}

a6aae819960253d6d0d9cac13e15d9e1614c5390
wei <shps951002@gmail.com>
2021-03-14 20:40:23 +0800

    Release remove [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MiniExcelTests")]

--- src/MiniExcel/Properties/AssemblyInfo.cs
+++ src/MiniExcel/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
-﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MiniExcelTests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MiniExcelLibs.Tests")]
+﻿#if DEBUG
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MiniExcelTests")]
+#endif
+
 #if PLAT_SKIP_LOCALS_INIT
 [module: System.Runtime.CompilerServices.SkipLocalsInit]
 #endif

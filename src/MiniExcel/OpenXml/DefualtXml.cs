421bbabf83915dfbd894df47e16611abb4a975a1
wei <shps951002@gmail.com>
2021-03-25 16:10:24 +0800

    Support minify default openxml xml

--- src/MiniExcel/OpenXml/DefualtXml.cs
+++ src/MiniExcel/OpenXml/DefualtXml.cs
@@ -5,23 +5,23 @@ namespace MiniExcelLibs.OpenXml
 {
     internal static class DefualtXml
     {
-        internal const string DefaultRels = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        internal static string DefaultRels = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <Relationships xmlns=""http://schemas.openxmlformats.org/package/2006/relationships"">
     <Relationship Type=""http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"" Target=""/xl/workbook.xml"" Id=""Rfc2254092b6248a9"" />
 </Relationships>";
 
-        internal const string DefaultSheetXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        internal static string DefaultSheetXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <x:worksheet xmlns:x=""http://schemas.openxmlformats.org/spreadsheetml/2006/main"">
     <x:sheetData>
     </x:sheetData>
 </x:worksheet>";
-        internal const string DefaultWorkbookXmlRels = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        internal static string DefaultWorkbookXmlRels = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <Relationships xmlns=""http://schemas.openxmlformats.org/package/2006/relationships"">
     <Relationship Type=""http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet"" Target=""/xl/worksheets/sheet1.xml"" Id=""R1274d0d920f34a32"" />
     <Relationship Type=""http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles"" Target=""/xl/styles.xml"" Id=""R3db9602ace774fdb"" />
 </Relationships>";
 
-        internal const string DefaultStylesXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        internal static string DefaultStylesXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <x:styleSheet xmlns:x=""http://schemas.openxmlformats.org/spreadsheetml/2006/main"">
     <x:fonts>
         <x:font />
@@ -41,13 +41,25 @@ namespace MiniExcelLibs.OpenXml
     </x:cellXfs>
 </x:styleSheet>";
 
-        internal const string DefaultWorkbookXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+        internal static string DefaultWorkbookXml = @"<?xml version=""1.0"" encoding=""utf-8""?>
 <x:workbook xmlns:r=""http://schemas.openxmlformats.org/officeDocument/2006/relationships""
     xmlns:x=""http://schemas.openxmlformats.org/spreadsheetml/2006/main"">
     <x:sheets>
         <x:sheet xmlns:r=""http://schemas.openxmlformats.org/officeDocument/2006/relationships"" name=""Sheet1"" sheetId=""1"" r:id=""R1274d0d920f34a32"" />
     </x:sheets>
 </x:workbook>";
+
+        static DefualtXml()
+        {
+            DefaultRels = MinifyXml(DefaultRels);
+            DefaultWorkbookXml = MinifyXml(DefaultWorkbookXml);
+            DefaultStylesXml = MinifyXml(DefaultStylesXml);
+            DefaultWorkbookXmlRels = MinifyXml(DefaultWorkbookXmlRels);
+            DefaultSheetXml = MinifyXml(DefaultSheetXml);
+        }
+
+        private static string MinifyXml(string xml) => xml.Replace("    ", "").Replace("\r", "").Replace("\n", "").Replace("\t", "");
+
         public static Dictionary<string, ZipPackageInfo> GetDefaultFiles() => new Dictionary<string, ZipPackageInfo>()
         {
             { @"_rels/.rels",new ZipPackageInfo(DefualtXml.DefaultRels, "application/vnd.openxmlformats-package.relationships+xml")},

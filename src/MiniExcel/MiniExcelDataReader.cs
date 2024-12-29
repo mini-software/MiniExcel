74477f1714395e60867e7c77b81f45c3e1d0b335
Wei Lin <shps951002@gmail.com>
2024-12-29 12:48:49 +0800

    Refactor MiniExcelDataReader

--- src/MiniExcel/MiniExcelDataReader.cs
+++ src/MiniExcel/MiniExcelDataReader.cs
@@ -1,5 +1,6 @@
 ﻿namespace MiniExcelLibs
 {
+    using System;
     using System.Collections.Generic;
     using System.Data;
     using System.IO;
@@ -12,36 +13,38 @@
         private readonly List<string> _keys;
         private readonly Stream _stream;
         private bool _isFirst = true;
+        private bool _disposed = false;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MiniExcelDataReader"/> class.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <param name="useHeaderRow">Whether to use the header row.</param>
+        /// <param name="sheetName">The name of the sheet.</param>
+        /// <param name="excelType">The type of the Excel file.</param>
+        /// <param name="startCell">The start cell.</param>
+        /// <param name="configuration">The configuration.</param>
         internal MiniExcelDataReader(Stream stream, bool useHeaderRow = false, string sheetName = null, ExcelType excelType = ExcelType.UNKNOWN, string startCell = "A1", IConfiguration configuration = null)
         {
-            _stream = stream;
+            _stream = stream ?? throw new ArgumentNullException(nameof(stream));
             _source = MiniExcel.Query(_stream, useHeaderRow, sheetName, excelType, startCell, configuration).Cast<IDictionary<string, object>>().GetEnumerator();
-            var isNext = _source.MoveNext();
-            if (isNext)
+            if (_source.MoveNext())
             {
-                _keys = _source.Current.Keys.ToList();
+                _keys = _source.Current?.Keys.ToList() ?? new List<string>();
                 _fieldCount = _keys.Count;
             }
         }
 
-        /// <summary>
         /// <inheritdoc/>
-        /// </summary>
-        /// <param name="i"></param>
-        /// <returns></returns>
         public override object GetValue(int i)
         {
+            if (_source.Current == null)
+                throw new InvalidOperationException("No current row available.");
             return _source.Current[_keys[i]];
         }
 
-        /// <summary>
         /// <inheritdoc/>
-        /// </summary>
-        public override int FieldCount
-        {
-            get { return _fieldCount; }
-        }
+        public override int FieldCount => _fieldCount;
 
         /// <summary>
         /// <inheritdoc/>
@@ -57,37 +60,32 @@
             return _source.MoveNext();
         }
 
-        /// <summary>
         /// <inheritdoc/>
-        /// </summary>
-        /// <param name="i"></param>
-        /// <returns></returns>
         public override string GetName(int i)
         {
             return _keys[i];
         }
-
-        /// <summary>
         /// <inheritdoc/>
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public override int GetOrdinal(string name)
+        protected override void Dispose(bool disposing)
         {
-            var i = _keys.IndexOf(name);
-            return _keys.IndexOf(name);
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _stream?.Dispose();
+                }
+                _disposed = true;
+            }
+            base.Dispose(disposing);
         }
 
         /// <summary>
-        /// <inheritdoc/>
+        /// Disposes the object.
         /// </summary>
-        /// <param name="disposing"></param>
-        protected override void Dispose(bool disposing)
+        public new void Dispose()
         {
-            if (disposing)
-            {
-                _stream.Dispose();
-            }
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }

c2ef412b31541affc8c150ecf0aa79fee9d177d9
Nate <ntripp@workfirstcasualty.com>
2025-03-29 02:48:49 -0600

    Refactored how formats are copied. (#747)
    * Add conditional formatting support to Excel template
    
    - Introduced `ConditionalFormats` property in `XRowInfo` to store XML elements for conditional formatting.
    - Added `_cellRegex` for matching cell references and implemented `ParseConditionalFormatRanges` to extract conditional formatting ranges.
    - Defined `Range` struct and `ConditionalFormatRange` class for managing cell ranges and their associated nodes.
    - Updated `WriteSheetXml` to handle conditional formats, ensuring correct parsing, storage, and writing of these formats in the XML document.
    
    * Refactor conditional formatting range processing
    
    This commit simplifies the processing of ranges for conditional formatting by using LINQ's `Where` method to filter ranges that contain the specified row index. It also adjusts the `StartRow` and `EndRow` values to use `enumrowstart` and `enumrowend` directly, which may impact the defined range boundaries.
    
    ---------
    
    Signed-off-by: Nate <ntripp@workfirstcasualty.com>

--- src/MiniExcel/SaveByTemplate/ExcelOpenXmlTemplate.Impl.cs
+++ src/MiniExcel/SaveByTemplate/ExcelOpenXmlTemplate.Impl.cs
@@ -834,26 +834,17 @@ namespace MiniExcelLibs.OpenXml.SaveByTemplate
                         enumrowend = newRowIndex-1;
 
                         var conditionalFormats = conditionalFormatRanges.Where(cfr => cfr.Ranges.Any(r => r.ContainsRow(originRowIndex)));
-                        foreach (var conditionalFormat in conditionalFormats) {
+                        foreach (var conditionalFormat in conditionalFormats) 
+                        {
                             var newConditionalFormat = conditionalFormat.Node.Clone();
                             var sqref = newConditionalFormat.Attributes["sqref"];
-                            var ranges = conditionalFormat.Ranges.Select(r => 
-                            {
-                                if (r.ContainsRow(originRowIndex))
-                                {
-                                    return new Range() 
-                                    { 
-                                        StartColumn = r.StartColumn,
-                                        StartRow = enumrowstart + 1,
-                                        EndColumn = r.EndColumn,
-                                        EndRow = enumrowend + 1
-                                    };
-                                }
-                                else 
-                                {
-                                    return r;
-                                }
-                            }).ToList();
+                            var ranges = conditionalFormat.Ranges.Where(r => r.ContainsRow(originRowIndex)).Select(r => new Range() 
+                                { 
+                                    StartColumn = r.StartColumn,
+                                    StartRow = enumrowstart,
+                                    EndColumn = r.EndColumn,
+                                    EndRow = enumrowend
+                                }).ToList();
                             sqref.Value = string.Join(" ", ranges.Select(r => $"{ColumnHelper.GetAlphabetColumnName(r.StartColumn)}{r.StartRow}:{ColumnHelper.GetAlphabetColumnName(r.EndColumn)}{r.EndRow}"));
                             newConditionalFormatRanges.Remove(conditionalFormat);
                             newConditionalFormatRanges.Add(new ConditionalFormatRange {

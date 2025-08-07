382d0523eefaf73f13933b281f16119f55ac3b14
Michele Bastione <michele.bastione@gmail.com>
2025-08-07 23:42:09 +0200

    Rebasing changes on top of the latest refactoring and making some corrections
    Co-Authored-By: dazhu6666 <59462071+dazhu6666@users.noreply.github.com>
    Co-Authored-By: Wei Lin <shps951002@gmail.com>

---
+++ src/MiniExcel.Core/OpenXml/Styles/OpenXmlHeaderStyle.cs
@@ -0,0 +1,27 @@
+using System.Drawing;
+using MiniExcelLib.Core.Enums;
+
+namespace MiniExcelLib.Core.OpenXml.Styles;
+
+public class OpenXmlHeaderStyle
+{
+    /// <summary>
+    /// Whether to wrap the content of the header
+    /// </summary>
+    public bool WrapText { get; set; }
+
+    /// <summary>
+    /// The RGB background color in the filtered state
+    /// </summary>
+    public Color BackgroundColor { get; set; } = Color.FromArgb(0x284472C4);
+
+    /// <summary>
+    /// Horizontal alignment
+    /// </summary>
+    public HorizontalCellAlignment HorizontalAlignment { get; set; } = HorizontalCellAlignment.Left;
+
+    /// <summary>
+    /// Vertical alignment
+    /// </summary>
+    public VerticalCellAlignment VerticalAlignment { get; set; } = VerticalCellAlignment.Bottom;
+}

9847b6fee01827ffa4b00d954b84f6adffd21e6f
Michele Bastione <michele.bastione@gmail.com>
2025-09-10 02:17:52 +0200

    Mirrors the fix for issue 869 on the master branch (#873)
    * Mirrors the fix for issue 869 on the master branch
    * Fixing test and early return property set
    
    ---------
    
    Co-authored-by: RiRiSharp <30719799+RiRiSharp@users.noreply.github.com>

--- src/MiniExcel/IConfiguration.cs
+++ src/MiniExcel/IConfiguration.cs
@@ -15,5 +15,28 @@ namespace MiniExcelLibs
         ///     When exporting using DataReader, the data not in DynamicColumn will be filtered.
         /// </summary>
         public bool DynamicColumnFirst { get; set; } = false;
+        
+        /// <summary>
+        /// Specifies when and how DateTime values are converted to DateOnly values.
+        /// </summary>
+        public DateOnlyConversionMode DateOnlyConversionMode { get; set; }
+    }
+    
+    public enum DateOnlyConversionMode
+    {
+        /// <summary>
+        /// No conversion is applied and DateOnly values are not transformed.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Allows conversion from DateTime to DateOnly only if the time component is exactly midnight (00:00:00).
+        /// </summary>
+        RequireMidnight,
+
+        /// <summary>
+        /// Converts DateTime to DateOnly by ignoring the time part completely, assuming the time component is not critical.
+        /// </summary>
+        IgnoreTimePart
     }
 }

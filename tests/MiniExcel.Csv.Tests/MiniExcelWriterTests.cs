0878c69ed6ddf7034b4331910d780d785cacdebb
Sal <chunkymonkey_42@hotmail.com>
2025-10-04 20:53:49 -0400

    simplify csv test

--- tests/MiniExcel.Csv.Tests/MiniExcelWriterTests.cs
+++ tests/MiniExcel.Csv.Tests/MiniExcelWriterTests.cs
@@ -15,51 +15,45 @@ public class MiniExcelWriterTests
 
         // We need to use the file system because the CsvExporter automatically disposes the stream
         var tempFilePath = Path.GetTempFileName();
+        File.Delete(tempFilePath);
 
-        using (var fs = File.Create(tempFilePath))
+        var progress = new SimpleProgress();
+        var exporter = new CsvExporter();
+        var rowCounts = await exporter.ExportAsync(tempFilePath, dataTable, progress: progress);
+        Assert.Single(rowCounts);
+        Assert.Equal(3, rowCounts.First());
+
+        //Confirm the progress report is correct
+        var cellCount = dataTable.Columns.Count * dataTable.Rows.Count;
+        Assert.Equal(cellCount, progress.Value);
+
+        var importer = new CsvImporter();
+        var resultDataTable = importer.QueryAsDataTable(tempFilePath);
+
+        //Confirm the data is correct
+        Assert.Equal(dataTable.Rows.Count, resultDataTable.Rows.Count);
+        Assert.Equal(dataTable.Columns.Count, resultDataTable.Columns.Count);
+        for (var i = 0; i < dataTable.Rows.Count; i++)
         {
-
-            var progress = new SimpleProgress();
-            var exporter = new CsvExporter();
-            var rowCounts = await exporter.ExportAsync(fs, dataTable, progress: progress);
-            Assert.Single(rowCounts);
-            Assert.Equal(3, rowCounts.First());
-
-            //Confirm the progress report is correct
-            var cellCount = dataTable.Columns.Count * dataTable.Rows.Count;
-            Assert.Equal(cellCount, progress.Value);
-        }
-
-        using (var fs = File.OpenRead(tempFilePath))
-        {
-            var importer = new CsvImporter();
-            var resultDataTable = importer.QueryAsDataTable(fs);
-
-            //Confirm the data is correct
-            Assert.Equal(dataTable.Rows.Count, resultDataTable.Rows.Count);
-            Assert.Equal(dataTable.Columns.Count, resultDataTable.Columns.Count);
-            for (var i = 0; i < dataTable.Rows.Count; i++)
+            for (var j = 0; j < dataTable.Columns.Count; j++)
             {
-                for (var j = 0; j < dataTable.Columns.Count; j++)
+                if (dataTable.Columns[j].DataType == typeof(DateTime))
                 {
-                    if (dataTable.Columns[j].DataType == typeof(DateTime))
-                    {
-                        //We need to compare Dates properly as they will be formatted differently in CSV
-                        //Note: if dates have millisecond precision that will be lost when saving to CSV
-                        DateTime.TryParse(resultDataTable.Rows[i][j].ToString(), out var resultDate);
-                        Assert.Equal((DateTime)dataTable.Rows[i][j], resultDate);
-                    }
-                    else
-                    {
-                        //We compare string values because types change after writing and reading them back
-                        Assert.Equal(dataTable.Rows[i][j].ToString(), resultDataTable.Rows[i][j].ToString());
-                    }
+                    //We need to compare Dates properly as they will be formatted differently in CSV
+                    //Note: if dates have millisecond precision that will be lost when saving to CSV
+                    DateTime.TryParse(resultDataTable.Rows[i][j].ToString(), out var resultDate);
+                    Assert.Equal((DateTime)dataTable.Rows[i][j], resultDate);
+                }
+                else
+                {
+                    //We compare string values because types change after writing and reading them back
+                    Assert.Equal(dataTable.Rows[i][j].ToString(), resultDataTable.Rows[i][j].ToString());
                 }
             }
         }
     }
 
-    private class SimpleProgress: IProgress<int>
+    private class SimpleProgress : IProgress<int>
     {
         public int Value { get; private set; }
         public void Report(int value)

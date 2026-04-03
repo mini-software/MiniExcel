3b611a749566984b2e7e52eefe6f7484792d66d0
Michele Bastione <michele.bastione@gmail.com>
2026-04-03 16:14:15 +0200

    Removed superfluous whitespace
    Signed-off-by: Michele Bastione <michele.bastione@gmail.com>

--- src/MiniExcel.Core/Helpers/NetSatandardExtensions.cs
+++ src/MiniExcel.Core/Helpers/NetSatandardExtensions.cs
@@ -5,7 +5,7 @@ public static class NetStandardExtensions
 #if NETSTANDARD2_0
     public static TValue? GetValueOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue? defaultValue = default)
     {
-        return  dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
     }
 #endif
 }

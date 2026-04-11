1797aec845b6dfd03473526bb549d8387ca77bb6
Michele Bastione <michele.bastione@gmail.com>
2026-04-11 21:04:49 +0200

    Backports background color and text alignment style options from v2.x (#945)

--- src/MiniExcel/OpenXml/OpenXmlStyleOptions.cs
+++ src/MiniExcel/OpenXml/OpenXmlStyleOptions.cs
@@ -1,7 +1,38 @@
+using System.Drawing;
+
 namespace MiniExcelLibs.OpenXml
 {
     public class OpenXmlStyleOptions
     {
+        public OpenXmlHeaderStyle HeaderStyle { get; set; }
         public bool WrapCellContents { get; set; }
+        public HorizontalCellAlignment HorizontalAlignment { get; set; }
+        public VerticalCellAlignment VerticalAlignment { get; set; }
     }
+
+    public class OpenXmlHeaderStyle
+    {
+        /// <summary>
+        /// Whether to wrap the content of the header
+        /// </summary>
+        public bool WrapText { get; set; }
+
+        /// <summary>
+        /// The RGB background color in the filtered state
+        /// </summary>
+        public Color BackgroundColor { get; set; } = Color.FromArgb(0x284472C4);
+
+        /// <summary>
+        /// Horizontal alignment
+        /// </summary>
+        public HorizontalCellAlignment HorizontalAlignment { get; set; } = HorizontalCellAlignment.Left;
+
+        /// <summary>
+        /// Vertical alignment
+        /// </summary>
+        public VerticalCellAlignment VerticalAlignment { get; set; } = VerticalCellAlignment.Bottom;
+    }
+    
+    public enum HorizontalCellAlignment { Left, Center, Right }
+    public enum VerticalCellAlignment { Bottom, Center, Top }
 }

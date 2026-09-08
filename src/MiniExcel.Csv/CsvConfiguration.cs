da4d222cb57f1053fde5323987f9c473a0511cbf
Evgeny Valavin <evgeny.valavin@outlook.com>
2026-09-08 18:24:28 +0300

    Fix

--- src/MiniExcel.Csv/CsvConfiguration.cs
+++ src/MiniExcel.Csv/CsvConfiguration.cs
@@ -5,9 +5,11 @@ namespace MiniExcelLib.Csv;
 public class CsvConfiguration : MiniExcelBaseConfiguration
 {
     private static readonly Encoding DefaultEncoding = new UTF8Encoding(true);
+    private Func<Stream, StreamReader>? _streamReaderFunc;
+    private Func<Stream, StreamWriter>? _streamWriterFunc;
 
     internal static CsvConfiguration Default => new();
-    
+
     public char Seperator { get; set; } = ',';
     public string NewLine { get; set; } = "\r\n";
     public bool ReadLineBreaksWithinQuotes { get; set; } = true;
@@ -17,6 +19,15 @@ public class CsvConfiguration : MiniExcelBaseConfiguration
     public Func<string, string[]>? SplitFn { get; set; }
 
     // we leave the stream open by default and close it in the CsvReader unless the consumer decides to keep it open.
-    public Func<Stream, StreamReader> StreamReaderFunc { get; set; } = stream => new StreamReader(stream, DefaultEncoding, true, 1024, true);
-    public Func<Stream, StreamWriter> StreamWriterFunc { get; set; } = stream => new StreamWriter(stream, DefaultEncoding);
+    public Func<Stream, StreamReader> StreamReaderFunc
+    {
+        get => _streamReaderFunc ??= stream => new StreamReader(stream, DefaultEncoding, true, BufferSize, leaveOpen: true);
+        set => _streamReaderFunc = value;
+    }
+
+    public Func<Stream, StreamWriter> StreamWriterFunc
+    {
+        get => _streamWriterFunc ??= stream => new StreamWriter(stream, DefaultEncoding, BufferSize);
+        set => _streamWriterFunc = value;
+    }
 }

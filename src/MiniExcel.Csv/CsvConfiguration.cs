aca53de1b3167f6e2c36d0008835446b85e4d0f6
Michele Bastione <michele.bastione@gmail.com>
2026-09-08 20:11:54 +0200

    Edited custom reader and writer functions' properties to use the field keyword instead of explicit backing fields

--- src/MiniExcel.Csv/CsvConfiguration.cs
+++ src/MiniExcel.Csv/CsvConfiguration.cs
@@ -5,8 +5,6 @@ namespace MiniExcelLib.Csv;
 public class CsvConfiguration : MiniExcelBaseConfiguration
 {
     private static readonly Encoding DefaultEncoding = new UTF8Encoding(true);
-    private Func<Stream, StreamReader>? _streamReaderFunc;
-    private Func<Stream, StreamWriter>? _streamWriterFunc;
 
     internal static CsvConfiguration Default => new();
 
@@ -21,13 +19,13 @@ public class CsvConfiguration : MiniExcelBaseConfiguration
     // we leave the stream open by default and close it in the CsvReader unless the consumer decides to keep it open.
     public Func<Stream, StreamReader> StreamReaderFunc
     {
-        get => _streamReaderFunc ??= stream => new StreamReader(stream, DefaultEncoding, true, BufferSize, leaveOpen: true);
-        set => _streamReaderFunc = value;
+        get => field ??= stream => new StreamReader(stream, DefaultEncoding, true, BufferSize, leaveOpen: true);
+        set;
     }
 
     public Func<Stream, StreamWriter> StreamWriterFunc
     {
-        get => _streamWriterFunc ??= stream => new StreamWriter(stream, DefaultEncoding, BufferSize);
-        set => _streamWriterFunc = value;
+        get => field ??= stream => new StreamWriter(stream, DefaultEncoding, BufferSize);
+        set;
     }
 }
